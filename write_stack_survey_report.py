d169ef8953965a485dfc1e368339d961847b4fd1
Andrew Mundy <awmundy@gmail.com>
2022-08-20 19:03:48 -0400

    some utility functions, rename wrapping

--- write_stack_survey_report.py
+++ write_stack_survey_report.py
@@ -68,6 +68,153 @@ def safe_rename(df, columns):
 
     return df
 
+def get_dummies_from_series_with_list_values(ser):
+    '''Get dummies (aka multihot encoding) from a pandas series that contains lists'''
+    ser = ser.copy()
+
+    # build dummies
+    mlb = MultiLabelBinarizer()
+    dummies_col_names = [x for x in mlb.fit(ser).classes_]
+    dummies = pd.DataFrame(mlb.fit_transform(ser), columns=dummies_col_names)
+
+    return dummies
+
+def get_rename_dict(year):
+    renames = {'2022': {'OpSysProfessional use': 'OpSysProfessionalUse', # no space
+                        },
+               '2021': {'OpSys': 'OpSysProfessionalUse',
+                        },
+               '2020': {'DatabaseWorkedWith': 'DatabaseHaveWorkedWith',
+                        'DatabaseDesireNextYear': 'DatabaseWantToWorkWith',
+                        'LanguageWorkedWith': 'LanguageHaveWorkedWith',
+                        'LanguageDesireNextYear': 'LanguageWantToWorkWith',
+                        'NEWCollabToolsWorkedWith': 'NEWCollabToolsHaveWorkedWith',
+                        'NEWCollabToolsDesireNextYear': 'NEWCollabToolsWantToWorkWith',
+                        'OpSys': 'OpSysProfessionalUse',
+                        'PlatformWorkedWith': 'PlatformHaveWorkedWith',
+                        'PlatformDesireNextYear': 'PlatformWantToWorkWith',
+                        'MiscTechWorkedWith': 'MiscTechHaveWorkedWith',
+                        'MiscTechDesireNextYear': 'MiscTechWantToWorkWith',
+                        'WebframeWorkedWith': 'WebframeHaveWorkedWith',
+                        'WebframeDesireNextYear': 'WebframeWantToWorkWith',
+                        },
+               '2019': {'DatabaseWorkedWith': 'DatabaseHaveWorkedWith',
+                        'DatabaseDesireNextYear': 'DatabaseWantToWorkWith',
+                        'LanguageWorkedWith': 'LanguageHaveWorkedWith',
+                        'LanguageDesireNextYear': 'LanguageWantToWorkWith',
+                        'OpSys': 'OpSysProfessionalUse',
+                        'PlatformWorkedWith': 'PlatformHaveWorkedWith',
+                        'PlatformDesireNextYear': 'PlatformWantToWorkWith',
+                        'MiscTechWorkedWith': 'MiscTechHaveWorkedWith',
+                        'MiscTechDesireNextYear': 'MiscTechWantToWorkWith',
+                        'WebFrameWorkedWith': 'WebframeHaveWorkedWith',
+                        'WebFrameDesireNextYear': 'WebframeWantToWorkWith',},
+               '2018': {'DatabaseWorkedWith': 'DatabaseHaveWorkedWith',
+                        'DatabaseDesireNextYear': 'DatabaseWantToWorkWith',
+                        'LanguageWorkedWith': 'LanguageHaveWorkedWith',
+                        'LanguageDesireNextYear': 'LanguageWantToWorkWith',
+                        'OperatingSystem': 'OpSysProfessionalUse',
+                        'PlatformWorkedWith': 'PlatformHaveWorkedWith',
+                        'PlatformDesireNextYear': 'PlatformWantToWorkWith',},
+               }
+    rename_dict = renames[year]
+
+    return rename_dict
+
+def convert_col_values_to_lists(ser, delimiter=';'):
+    '''
+    :param ser: series that is the column of a dataframe containing list-like values
+    :param delimiter: delimiter dividing elements of the list_like values in the series
+    '''
+    ser = ser.str.split(delimiter)
+
+    return ser
+
+def fillna_col_of_lists(ser):
+    '''
+    :param ser: series that is the column of a dataframe
+    '''
+    # fill nas with an empty list, standard methods like fillna dont allow this
+    ser = ser.fillna('').apply(list)
+
+    return ser
+
+def assert_no_duplicate_cols(df):
+    dupe_cols = df.columns[df.columns.duplicated()]
+    if len(dupe_cols) > 0:
+        print(dupe_cols)
+        raise Exception(f'Duplicate columns exist in the dataframe: {dupe_cols}')
+
+def convert_to_list_if_not_list(obj):
+    if (type(obj) != list) & (obj != None):
+        out = [obj]
+    else:
+        out = obj
+    return out
+
+def safe_merge(left, right, how='inner', on=None, left_on=None, right_on=None,
+               left_index=False, right_index=False, sort=False,
+               suffixes=None, copy=True, indicator=False, validate=None):
+    '''Pandas merge wrapper with safety features and forced explicitness
+       Features:
+            - Suffixes defaults to None. If None and there are non-merge-key columns
+              with the same name in left and right, throws an error  '''
+    assert_no_duplicate_cols(left)
+    assert_no_duplicate_cols(right)
+
+    # convert "ons" to lists for easier handling
+
+    on = convert_to_list_if_not_list(on)
+    left_on = convert_to_list_if_not_list(left_on)
+    right_on = convert_to_list_if_not_list(right_on)
+
+    if ((left_on is not None) & (right_on is None)) | ((right_on is not None) & (left_on is None)):
+        raise Exception('If left_on or right_on is specified, the other must be as well')
+
+    if on is None and left_on is None:
+        raise Exception('Either on must be specified or left_on/right_on must be specified')
+
+    if on is not None and left_on is not None:
+        raise Exception('on and left_on/right_on cannot be specified simultaneously')
+
+    if on:
+        left_non_merge_keys = [x for x in left if x not in on]
+        right_non_merge_keys = [x for x in right if x not in on]
+    else:
+        left_non_merge_keys = [x for x in left if x not in left_on]
+        right_non_merge_keys = [x for x in right if x not in right_on]
+
+
+    shared_non_merge_keys = [x for x in left_non_merge_keys if x in right_non_merge_keys]
+    if suffixes == None:
+        if len(shared_non_merge_keys) > 0:
+            raise Exception(f'Suffixes argument is None but the following '
+                            f'non-merge-key columns are shared between the '
+                            f'left and right dataframes: {shared_non_merge_keys}')
+        # change suffixes to the default for pandas to minimize dependency
+        # on merge api: at this point suffixes won't be doing anything
+        suffixes = ('_x', '_y')
+
+    out = pd.merge(left, right, how, on, left_on, right_on, left_index,
+                   right_index, sort, suffixes, copy, indicator, validate)
+
+    return out
+
+    # categories = df[col].str.split(';').explode().unique().tolist()
+    # for null_val in [np.nan, None]:
+    #     if null_val in categories:
+    #         categories.remove(null_val)
+    # for category in categories:
+    #     msk = df[col].str.contains(category)
+
+def transpose_to_long_year_wide_category(plot_df):
+    # pivot to be wide by category and long by year
+    plot_df = plot_df.transpose().reset_index()
+
+    # move column headers out of the first row
+    plot_df.columns = plot_df.head(1).values.tolist()[0]
+    plot_df.drop(axis=0, index=0, inplace=True)
+    plot_df.rename(columns={'category': 'year'}, inplace=True)
 
 
 max_survey_year = 2022
